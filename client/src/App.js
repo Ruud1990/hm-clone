055d106e0157e7cc8589c91528b4201f2ee9d6ef
Krzysztof Prońko <pronko.krzysztof@gmail.com>
2022-12-25 14:14:29 +0100

    added navbar view

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-import './App.css';
 import Home from './pages/Home/Home';
 import Products from './pages/Products/Products';
 import Product from './pages/Product/Product';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
+import './App.css';
 
 const Layout = () => {
   return (
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,60 @@
 import React from 'react'
+import { AiOutlineArrowDown, AiOutlineSearch, AiOutlineShoppingCart } from 'react-icons/ai'
+import { GoPerson } from 'react-icons/go'
+import { MdFavorite } from 'react-icons/md'
+import {Link} from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='navbar'>
+      <div className='navbar-wrapper'>
+        <div className='navbar-left'>
+          <div className='navbar-item'>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Flag_of_the_United_Kingdom.png/1200px-Flag_of_the_United_Kingdom.png?20080216232030' alt='eng-flag'/>
+            <AiOutlineArrowDown />
+          </div>
+          <div className='navbar-item'>
+            <span>USD</span>
+            <AiOutlineArrowDown />
+          </div>
+          <div className='navbar-item'>
+            <Link className='link' to='products/1'>Women</Link>
+          </div>
+          <div className='navbar-item'>
+            <Link className='link' to='products/2'>Men</Link>
+          </div>
+          <div className='navbar-item'>
+            <Link className='link' to='products/3'>Children</Link>
+          </div>
+        </div>
+        <div className='navbar-center'>
+          <Link className='link' to='/'>HM Clone Store</Link>
+        </div>
+        <div className='navbar-right'>
+        <div className='navbar-item'>
+          <Link className='link' to='/'>Home</Link>
+        </div>
+        <div className='navbar-item'>
+          <Link className='link' to='/'>About</Link>
+        </div>
+        <div className='navbar-item'>
+          <Link className='link' to='/'>Contact</Link>
+        </div>
+        <div className='navbar-item'>
+          <Link className='link' to='/'>Store</Link>
+        </div>
+        <div className='navbar-icons'>
+          <AiOutlineSearch/>
+          <GoPerson/>
+          <MdFavorite/>
+          <div className='cart-icon'>
+          <AiOutlineShoppingCart/>
+          <span>0</span>
+          </div>
+        </div>
+        </div>
+      </div>
+    </nav>
   )
 }
 
