993994fc145f5be59ac49e385d86e21d906527c8
Krzysztof Prońko <pronko.krzysztof@gmail.com>
2022-12-27 14:42:21 +0100

    added featured products components

---
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import Card from '../Card/Card'
+
+const FeaturedProducts = () => {
+
+    const data = [
+        {
+            id: 1,
+            img: 'https://images.unsplash.com/photo-1492707892479-7bc8d5a4ee93?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80',
+            img2: 'https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=420&q=80',
+            title: 'ssc',
+            oldPrice: 22,
+            price: 12,
+        },
+        {
+            id: 2,
+            img: 'https://images.unsplash.com/photo-1492707892479-7bc8d5a4ee93?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80',
+            img2: 'https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=420&q=80',
+            title: 'ssc',
+            oldPrice: 22,
+            price: 12,
+            isNew: true,
+        },
+        {
+            id: 3,
+            img: 'https://images.unsplash.com/photo-1492707892479-7bc8d5a4ee93?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80',
+            img2: 'https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=420&q=80',
+            title: 'ssc',
+            oldPrice: 22,
+            price: 12,
+        },
+        {
+            id: 4,
+            img: 'https://images.unsplash.com/photo-1492707892479-7bc8d5a4ee93?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80',
+            img2: 'https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=420&q=80',
+            title: 'ssc',
+            oldPrice: 22,
+            price: 12,
+        }
+    ]
+  return (
+    <div className='featuredProducts'>
+        <div className='featuredProducts-top'>
+            <h1>products</h1>
+            <p>lorem ipsum dolor sit amet</p>
+        </div>
+        <div className='featuredProducts-bottom'>
+            {data.map(item => (
+                <Card item={item} key={item.id} />
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default FeaturedProducts
